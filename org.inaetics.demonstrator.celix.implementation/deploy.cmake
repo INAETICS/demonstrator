8b6cd6f78b6ceaebe161180b12db3a5575ef9d3f
Bjoern Petri <bpetri@apache.org>
2015-08-21 06:52:21 +0200

    Added producer_endpoint to deployment

--- org.inaetics.demonstrator.celix.implementation/deploy.cmake
+++ org.inaetics.demonstrator.celix.implementation/deploy.cmake
@@ -9,6 +9,7 @@ deploy("inaetics-demonstrator-all" BUNDLES
         org.inaetics.demonstrator.api.datastore.DataStore_endpoint
         org.inaetics.demonstrator.api.datastore.DataStore_proxy
         org.inaetics.demonstrator.api.producer.Producer
+	org.inaetics.demonstrator.api.producer.Producer_endpoint
         org.inaetics.demonstrator.api.processor.Processor
       )
 
@@ -44,6 +45,7 @@ deploy("inaetics-demonstrator-all" BUNDLES
         ENDPOINTS
         org.inaetics.demonstrator.api.queue.SampleQueue_proxy
         org.inaetics.demonstrator.api.stats.StatsProvider_endpoint
+	org.inaetics.demonstrator.api.producer.Producer_endpoint
       )
 
   deploy("inaetics-demonstrator-processor" BUNDLES
