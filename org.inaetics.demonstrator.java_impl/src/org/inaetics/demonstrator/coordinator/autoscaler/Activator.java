dc2599d23764babde56b7f9975c25dd37300e251
Marc Sluiter <marc.sluiter@luminis.eu>
2015-09-08 11:53:49 +0200

    make autoscaler slope configurable

--- org.inaetics.demonstrator.java_impl/src/org/inaetics/demonstrator/coordinator/autoscaler/Activator.java
+++ org.inaetics.demonstrator.java_impl/src/org/inaetics/demonstrator/coordinator/autoscaler/Activator.java
@@ -23,10 +23,11 @@ import org.inaetics.demonstrator.api.coordinator.CoordinatorService;
 import org.inaetics.demonstrator.api.stats.StatsProvider;
 import org.osgi.framework.BundleContext;
 import org.osgi.framework.Constants;
+import org.osgi.service.cm.ManagedService;
 import org.osgi.service.log.LogService;
 
 public class Activator extends DependencyActivatorBase {
-    private static final String PID = "org.inaetics.demonstrator.coordinator.autoscalar.simple";
+    private static final String PID = "org.inaetics.demonstrator.coordinator.autoscaler.simple";
 
     @Override
     public void init(BundleContext context, DependencyManager manager) throws Exception {
@@ -34,6 +35,7 @@ public class Activator extends DependencyActivatorBase {
         props.put(Constants.SERVICE_PID, PID);
 
         manager.add(createComponent()
+        	.setInterface(ManagedService.class.getName(), props)
             .setImplementation(AutoScalerImpl.class)
             .add(createServiceDependency().setService(CoordinatorService.class).setRequired(true))
             .add(createServiceDependency().setService(StatsProvider.class, "(type=queue)").setRequired(true))
