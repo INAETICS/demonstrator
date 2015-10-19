a5f46ad3756970dc8e92309fe65754b724ad8a99
Marc Sluiter <marc.sluiter@luminis.eu>
2015-10-19 17:42:23 +0200

    use ManagedService for clusterinfo config

--- org.inaetics.demonstrator.java_impl/src/org/inaetics/demonstrator/clusterinfo/Activator.java
+++ org.inaetics.demonstrator.java_impl/src/org/inaetics/demonstrator/clusterinfo/Activator.java
@@ -3,42 +3,73 @@
  */
 package org.inaetics.demonstrator.clusterinfo;
 
+import java.util.Dictionary;
 import java.util.Properties;
 
 import org.amdatu.kubernetes.Kubernetes;
+import org.apache.felix.dm.Component;
 import org.apache.felix.dm.DependencyActivatorBase;
 import org.apache.felix.dm.DependencyManager;
 import org.inaetics.demonstrator.api.clusterinfo.ClusterInfo;
 import org.osgi.framework.BundleContext;
+import org.osgi.framework.Constants;
+import org.osgi.service.cm.ConfigurationException;
+import org.osgi.service.cm.ManagedService;
 import org.osgi.service.log.LogService;
 import org.osgi.service.remoteserviceadmin.RemoteConstants;
 
-public class Activator extends DependencyActivatorBase{
+public class Activator extends DependencyActivatorBase implements ManagedService {
 
-	/*
-	 * Needed only in case of ManagedService
-	 * private static final String PID = "ClusterInfo";
-	 */
-	private ClusterInfoImpl service = null; 
-
+    public static final String PID = "org.inaetics.demonstrator.clusterinfo";
+	private DependencyManager m_manager;
+	
+	private volatile Component m_component;
+	
 	@Override
 	public void init(BundleContext context, DependencyManager manager) throws Exception {
-		String iface = ClusterInfo.class.getName();
+		m_manager = manager;
 		
-		ClusterInfoConfig config = new ClusterInfoConfig();
-		service = new ClusterInfoImpl(config);
+		Properties props = new Properties();
+		props.put(Constants.SERVICE_PID, PID);
+		manager.add(createComponent()
+				.setInterface(ManagedService.class.getName(), props)
+				.setImplementation(this)
+		);
+	}
+	
+	@Override
+	public void updated(Dictionary<String, ?> properties) throws ConfigurationException {
+		try {
+			unregisterClusterInfo();
+			ClusterInfoConfig config = new ClusterInfoConfig(properties);
+			registerClusterInfo(config);
+		}
+		catch (Exception e) {
+			throw new ConfigurationException(null, "error reading config", e);
+		}		
+	}
+
+	private void registerClusterInfo(ClusterInfoConfig config) {
+		String iface = ClusterInfo.class.getName();
 
 		Properties props = new Properties();
-		//props.put(Constants.SERVICE_PID, PID);
 		props.put(RemoteConstants.SERVICE_EXPORTED_INTERFACES, iface);
 		props.put("type", "memory");
 
-		manager.add(createComponent()
+		m_component = createComponent()
 				.setInterface(iface, props)
-				.setImplementation(service)
+				.setImplementation(new ClusterInfoImpl(config))
 				.add(createServiceDependency().setService(Kubernetes.class).setRequired(true))
 				.add(createServiceDependency().setService(LogService.class).setRequired(false))
-				);
+		;
+		m_manager.add(m_component);
+	}
+	
+	private void unregisterClusterInfo() {
+		if (m_component != null) {
+			m_manager.remove(m_component);
+			m_component = null;
+		}
 	}
 
 }
--- org.inaetics.demonstrator.java_impl/src/org/inaetics/demonstrator/clusterinfo/ClusterInfoConfig.java
+++ org.inaetics.demonstrator.java_impl/src/org/inaetics/demonstrator/clusterinfo/ClusterInfoConfig.java
@@ -3,34 +3,76 @@
  */
 package org.inaetics.demonstrator.clusterinfo;
 
+import java.util.Dictionary;
+
 /**
  * Configuration values for cluster info
  */
 public class ClusterInfoConfig {
 	
+	private static final String KEY_CADVISOR_PORT = "cAdvisorPort";	
+	private static final String KEY_ETCD_ENDPOINT = "etcdEndpoint";	
+	private static final String KEY_FLEET_SELECTION_KEY = "fleetSelectionKey";	
+	private static final String KEY_FLEET_SELECTION_VALUE = "fleetSelectionValue";	
+	private static final String KEY_UPDATE_PERIOD = "updatePeriod";	
+
 	private final static int DEFAULT_CADVISOR_PORT = 4194;
 	private final static String DEFAULT_ETCD_ENDPOINT = "172.17.8.20:2379";
     private final static String DEFAULT_FLEET_SELECTION_KEY = "role";
     private final static String DEFAULT_FLEET_SELECTION_VALUE = "cluster";
-	private final static int DEFAULT_UPDATE_PERIOD = 10;
+	private final static int DEFAULT_UPDATE_PERIOD = 30;
+
+	private final int m_cAdvisorPort;
+	private final String m_etcdEndpoint;
+	private final String m_fleetSelectionKey;
+	private final String m_fleetSelectionValue;
+	private final int m_updatePeriod;
+	
+	public ClusterInfoConfig(Dictionary<String, ?> properties) {
+		m_cAdvisorPort = getIntValue(properties, KEY_CADVISOR_PORT, DEFAULT_CADVISOR_PORT);
+		m_etcdEndpoint = getStringValue(properties, KEY_ETCD_ENDPOINT, DEFAULT_ETCD_ENDPOINT);
+		m_fleetSelectionKey = getStringValue(properties, KEY_FLEET_SELECTION_KEY, DEFAULT_FLEET_SELECTION_KEY);
+		m_fleetSelectionValue = getStringValue(properties, KEY_FLEET_SELECTION_VALUE, DEFAULT_FLEET_SELECTION_VALUE);
+		m_updatePeriod = getIntValue(properties, KEY_UPDATE_PERIOD, DEFAULT_UPDATE_PERIOD);
+	}
+	
+	private String getStringValue(Dictionary<String, ?> properties, String key, String defaultValue) {
+		Object value = properties.get(key);
+		if (value != null) {
+			return value.toString();
+		}
+		return defaultValue;
+	}
+
+	private int getIntValue(Dictionary<String, ?> properties, String key, int defaultValue) {
+		Object value = properties.get(key);
+		if (value != null) {
+			try {
+				return Integer.parseInt(value.toString());
+			}
+			catch (Exception e) {
+			}
+		}
+		return defaultValue;
+	}
 
 	public int getCadvisorPort() {
-		return DEFAULT_CADVISOR_PORT;
+		return m_cAdvisorPort;
 	}
 
 	public String getEtcdEndpoint() {
-		return DEFAULT_ETCD_ENDPOINT;
+		return m_etcdEndpoint;
 	}
 
 	public String getFleetSelectionKey() {
-		return DEFAULT_FLEET_SELECTION_KEY;
+		return m_fleetSelectionKey;
 	}
 
 	public String getFleetSelectionValue() {
-		return DEFAULT_FLEET_SELECTION_VALUE;
+		return m_fleetSelectionValue;
 	}
 	
 	public int getUpdatePeriod() {
-		return DEFAULT_UPDATE_PERIOD;
+		return m_updatePeriod;
 	}
 }
